ca639fd7d3412ce45bccf2c7e30af9f0e113e7bc
OEM Configuration (temporary user) <albertaugustine1477@gmail.com>
2021-03-30 18:07:46 +0530

    Edited file

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="post">
+		<label>Name:</label><input type="text" name="name"><br><br>
+		<input type="submit" value="submit">
+	</form>
+  <?php
+    $conn = mysqli_connect("localhost", "root", "user123", "albert");
+    $name = $_POST['name'];
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO name_list (name)VALUES ('$name')";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close(); 
+  ?>
+  <form method="post">
+    <label>Delete:</label><input type="text" name="delete"><br><br>
+    <input type="submit" value="submit1">
+  </form>
+</body>
+</html> 
